0bcee61b8619aa33a84036a6789a88d99ab3488b
FelixVita <marnikaff@gmail.com>
2021-08-18 18:04:03 +0200

    Refactor release_mail_generator

--- who-are-you.py
+++ who-are-you.py
@@ -1,17 +1,10 @@
-import sys
-
-print("argument {}".format(sys.argv[1]))
-print("changelog 1")
-print("changelog 2")
-print("changelog 3")
-print("howdy doody")
-x = 3
-print(f"awesome {str(x)}")
-
 import getpass
 import platform
+
 username = getpass.getuser()
 hostname = platform.node()
+
 print(f"Hello there, {username}@{hostname}")
+
 if username.startswith("runner"):
     print("HEY RUNNER!")
