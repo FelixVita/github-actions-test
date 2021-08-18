a85eaccbf4d82f7f1e52bbfff9860f1dd8d1d1d9
FelixVita <marnikaff@gmail.com>
2021-08-18 18:09:40 +0200

    Does it still fail (runnerS)

--- greeting.py
+++ greeting.py
@@ -5,7 +5,7 @@ import platform
 username = getpass.getuser()
 hostname = platform.node()
 
-if username.startswith('runner'):
+if username.startswith('runners'):
     print('a' + 1)
 else:
     print('a' + str(1))
